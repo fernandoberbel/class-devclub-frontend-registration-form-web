6ad1a940c5475e82ced190d820e037384e413d4f
fernandoberbel <fberbel1000@gmail.com>
2024-06-24 00:32:45 -0300

    feat: validation with zod

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
@@ -10,11 +11,28 @@ import {
 } from "./components/ui/select";
 import "./styles/global.css";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  company: z.string(),
+  email: z.string().email(),
+  dateOfBirth: z.object({
+    month: z.string().optional(),
+    day: z.string().optional(),
+    year: z.string().optional(),
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export function App() {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, formState } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
-  function onSubmit(data: any) {
+  function onSubmit(data: FormData) {
     console.log(data);
   }
 
@@ -22,32 +40,60 @@ export function App() {
     <div className="flex h-screen w-full items-center justify-center bg-zinc-100">
       <div className="w-full max-w-2xl rounded-md bg-white p-8 shadow">
         <h1 className="text-center text-2xl font-bold">Registration</h1>
-        <form className="mt-8 flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)} noValidate>
+        <form
+          className="mt-8 flex flex-col gap-6"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
           <div className="grid grid-cols-2 gap-4">
             <div>
               <Label>First name</Label>
               <Input type="text" {...register("firstName")} />
+
+              {formState.errors.firstName?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.firstName?.message}
+                </span>
+              )}
             </div>
             <div>
               <Label>Last name</Label>
               <Input type="text" {...register("lastName")} />
+
+              {formState.errors.lastName?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.lastName?.message}
+                </span>
+              )}
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
             <div>
               <Label>E-mail</Label>
               <Input type="email" {...register("email")} />
+
+              {formState.errors.email?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.email?.message}
+                </span>
+              )}
             </div>
             <div>
               <Label>Company</Label>
               <Input type="text" {...register("company")} />
+
+              {formState.errors.company?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.company?.message}
+                </span>
+              )}
             </div>
           </div>
 
           <div className="grid grid-cols-3 items-end gap-4">
             <div>
               <Label>Date of birth</Label>
-              <Select {...register("dayOfBirth.month")}>
+              <Select {...register("dateOfBirth.month")}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Month" />
                 </SelectTrigger>
@@ -64,9 +110,15 @@ export function App() {
                     })}
                 </SelectContent>
               </Select>
+
+              {formState.errors.dateOfBirth?.month?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.dateOfBirth?.month?.message}
+                </span>
+              )}
             </div>
             <div>
-              <Select {...register("dayOfBirth.day")}>
+              <Select {...register("dateOfBirth.day")}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Day" />
                 </SelectTrigger>
@@ -83,9 +135,15 @@ export function App() {
                     })}
                 </SelectContent>
               </Select>
+
+              {formState.errors.dateOfBirth?.day?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.dateOfBirth?.day?.message}
+                </span>
+              )}
             </div>
             <div>
-              <Select {...register("dayOfBirth.year")}>
+              <Select {...register("dateOfBirth.year")}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Year" />
                 </SelectTrigger>
@@ -102,6 +160,12 @@ export function App() {
                     })}
                 </SelectContent>
               </Select>
+
+              {formState.errors.dateOfBirth?.year?.message && (
+                <span className="text-xs text-red-500">
+                  {formState.errors.dateOfBirth?.year?.message}
+                </span>
+              )}
             </div>
           </div>
 
